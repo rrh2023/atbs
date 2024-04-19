1d1aa99d99f9be0de438979f3069b640ef12d1e4
Roberto <rrh2023@bu.edu>
2024-04-18 22:57:56 -0400

    first few minutes of section 7

--- automate_the_boring_stuff/dictionaries.py
+++ automate_the_boring_stuff/dictionaries.py
@@ -1,1 +1,20 @@
 myCat = {'size': 'fat', 'color':'gray', 'disposition':'loud'}
+myCat['size']
+# 'fat'
+'My cat has ' + myCat['color'] + ' fur.'
+# 'My cat has gray fur.'
+spam = {12345: 'Luggage combination', 42: 'The Answer'}
+[1,2,3] == [3,2,1] 
+# False
+eggs = {'name':'Zophie', 'species': 'cat', 'age':8}
+ham = {'species':'cat', 'name':'Zophie', 'age':8}
+eggs == ham
+# True
+eggs['color']
+# ERROR
+'name' in eggs
+# True
+eggs
+# {'name':'Zophie', 'species':'cat','age':8}
+'name' not in eggs
+# False
